2cdb04fab7a8ae6ad23f6c09fba47c1673a0ab5c
Thiago Souza <thiagosouza@cos.ufrj.br>
2024-10-07 20:32:03 -0300

    versão inicial

---
+++ demo/src/main/java/com/empresa/projeto/demo/api/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.empresa.projeto.demo.api.controller;
+
+import com.empresa.projeto.demo.model.User;
+import com.empresa.projeto.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id){
+        Optional<User> user = userService.getUser(id);
+        return (User) user.orElse(null);
+    }
+}
+
